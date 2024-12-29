8fe5075402b50738084d62aab1cddadee069151d
Mikhail Galitskiy <mikhail.galitskiy@gmail.com>
2024-12-29 11:51:27 +0300

    Definitions

---
+++ information/definitions.py
@@ -0,0 +1,132 @@
+
+MEANINGS = [
+        'mm-what-you-sell',
+        'mm-why-somebody-buy-it',
+        'mm-which-problem-product-solves',
+        'mm-why-they-buy-exactly-your-product',
+        'mm-from-which-parts-product-consist-of',
+        'mm-product-working-principle',
+        'mm-service-stages',
+        'mm-product-advantages',
+        'mm-product-disadvantages',
+        'mm-product-package',
+        'mm-product-meterials',
+        'mm-product-sources',
+        'mm-product-production-where',
+        'mm-product-production-by-whom',
+        'mm-product-delivery-how',
+        'mm-product-storage-how',
+        'mm-product-usage-advantages',
+        'mm-product-usage-disadvantages',
+        'mm-name-of-company',
+        'mm-field-of-activity',
+        'mm-website',
+        'mm-products-and-services',
+        'mm-client-categories',
+        'mm-countries-of-activity',
+        'mm-sales-channels',
+        'mm-what-brought-you-to-this',
+        'mm-story-by-steps-and-stages',
+        'mm-numbers-saled-items',
+        'mm-numbers-employees',
+        'mm-numbers-clients',
+        'mm-numbers-offices',
+        'mm-numbers-visitors',
+        'mm-numbers-subscribers',
+        'mm-numbers-scale',
+        'mm-numbers-time',
+        'mm-company-public-founders',
+        'mm-company-age',
+        'mm-key-projects',
+        'mm-public-rewards',
+        'mm-who-communicates-with-clients',
+        'mm-employee-requirements',
+        'mm-clients-types',
+        'mm-clients-starts',
+        'mm-portfolio-most-expensive-project',
+        'mm-portfolio-most-ambitious-project',
+        'mm-portfolio-most-famous-project',
+        'mm-portfolio-most-unsusual-project',
+        'mm-portfolio-most-complicated-project',
+        'mm-most-popular-client-questions',
+        'mm-most-common-client-doubts',
+        'mm-main-clients-pain',
+        'mm-what-attracts-in-your-offer',
+        'mm-main-working-stages',
+        'mm-how-you-accompany-the-client',
+        'mm-best-promotions',
+        'mm-presents',
+        'mm-loyalty-program',
+        'mm-finance-conditions',
+        'mm-free-first-step',
+        'mm-equipment',
+        'mm-know-how',
+]
+
+
+PROPERTIES = {
+        'project-title' : '',
+        'website-skin' : 'blockflow',
+        'branding-color' : '#bf00ff',
+        'branding-gradient' : '',
+        'supported-language' : 'en-us',
+        'reference-website-url' : '',
+        'background-decoration' : 'none',
+        'page-background-color' : '#ffffff',
+        'light-color-threshold' : '0.55',
+        'image-generation' : 'true',
+        'image-background-remove' : 'true',
+        'image-background-model' : '',
+        'variables-use-query' : '',
+        'release-use-generated' : '',
+        'action-button-url' : '#',
+}
+
+
+RANDOM_SKINS = [
+        'blockflow',
+        'softcorners',
+]
+
+
+RANDOM_COLORS_WELL = [
+                                [['#3c5999','']], [['#0085ff','']], [['#55acef','']], [['#0077b5','']], [['#00b0f0','']], [['#017ee6','']], 
+                                [['#21759b','']], [['#1ab7ea','']], [['#0077b5','']], [['#4c75a3','']], [['#34465d','']], [['#410094','']], 
+                                [['#dd4c3a','']], [['#be081c','']], [['#cd211f','']], [['#eb4925','']], [['#ff5801','']], [['#b92c27','']], 
+                                [['#af0506','']], [['#df2029','']], [['#db562f','']], [['#ff6601','']], [['#ff3401','']], [['#f67d02','']], 
+                                [['#26d366','']], [['#08b83e','']], [['#01c300','']], [['#02b975','']], [['#00b48a','']], [['#3aaf86','']], 
+                                [['#e4405f','']], [['#ea4d89','']], [['#ff0185','']], [['#fa4877','']], [['#131418','']], [['#fffd01','']], 
+                        ]
+
+RANDOM_COLORS_GRADIENT = [
+                                [['#4158D0', 'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);']],
+                                [['#0093E9', 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);']],
+                                [['#8EC5FC', 'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);']],
+                                [['#D9AFD9', 'linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);']],
+                                [['#00DBDE', 'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%);']],
+                                [['#00DBDE', 'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%);']],
+                                [['#00DBDE', 'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%);']],
+                                [['#8BC6EC', 'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);']],
+                                [['#FFDEE9', 'linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%);']],
+                                [['#08AEEA', 'linear-gradient(0deg, #08AEEA 0%, #2AF598 100%);']],
+                                [['#08AEEA', 'linear-gradient(0deg, #08AEEA 0%, #2AF598 100%);']],
+                                [['#21D4FD', 'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);']],
+                                [['#3EECAC', 'linear-gradient(19deg, #3EECAC 0%, #EE74E1 100%);']],
+                                [['#FA8BFF', 'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%);']],
+                                [['#FF9A8B', 'linear-gradient(90deg, #FF9A8B 0%, #FF6A88 55%, #FF99AC 100%);']],
+                                [['#FBDA61', 'linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%);']],
+                                [['#F4D03F', 'linear-gradient(132deg, #F4D03F 0%, #16A085 100%);']],
+                                [['#74EBD5', 'linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%);']],
+                                [['#74EBD5', 'linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%);']],
+                                [['#FAD961', 'linear-gradient(90deg, #FAD961 0%, #F76B1C 100%);']],
+                                [['#FAD961', 'linear-gradient(90deg, #FAD961 0%, #F76B1C 100%);']],
+                                [['#FAD961', 'linear-gradient(90deg, #FAD961 0%, #F76B1C 100%);']],
+                        ]
+
+
+
+RANDOM_DECORATIONS = ['no', 'circle', 'curves', 'light-flow', 'net']
+
+
+
+
